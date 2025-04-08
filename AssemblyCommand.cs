9dbcf5d7959c1ec146aa619e5bdd8f68bb55397b
rysbrand <becca.leach325@gmail.com>
2025-04-08 10:48:26 -0500

    take 2 please work

---
+++ AssemblyCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minecraft
+{
+    public class AssemblyCommand
+    {
+        public string Command { get; set; }
+        public string Var1 { get; set; }
+        public string Var2 { get; set; }
+        public string Var3 { get; set; }
+    }
+}
